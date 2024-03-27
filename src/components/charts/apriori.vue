0ab41acc1f00c66da534d5aa5c174d82578d34fd
zhoubinxin <binxin.zhou@outlook.com>
2024-03-27 22:37:51 +0800

    apriori

--- src/components/charts/apriori.vue
+++ src/components/charts/apriori.vue
@@ -5,15 +5,25 @@
       </el-table-column>
       <el-table-column prop="support" label="支持度" sortable :sort-by="['support']">
       </el-table-column>
+      <el-table-column prop="confidence" label="置信度" sortable :sort-by="['confidence']">
+      </el-table-column>
     </el-table>
     <div v-else>
+      <el-result v-if="showAlert" icon="warning" title="" subTitle="无满足数据">
+
+      </el-result>
     </div>
+
   </div>
 </template>
 
 <script>
 export default {
   props: {
+    showAlert: {
+      type: Boolean,
+      required: true
+    },
     data: {
       type: Array,
       default: () => []
--- src/views/charts.vue
+++ src/views/charts.vue
@@ -13,14 +13,22 @@
                   <el-option v-for="item in algorithmLists" :key="item" :label="item" :value="item">
                   </el-option>
                 </el-select>
+                <el-select v-model="healthtype" clearable placeholder="请选择类别">
+                  <el-option v-for="item in healthtypes" :key="item" :label="item" :value="item">
+                  </el-option>
+                </el-select>
 
                 <div v-if="algorithm != ''" class="selectTitle">
                   <i class="el-icon-office-building"></i>&nbsp;参数
                 </div>
 
-                <el-input v-if="['apriori', 'LIG', 'FP-growth'].includes(algorithm)" v-model="supportThreshold" placeholder="请输入支持度阈值" size="small"
-                  :style="{ width: '200px' }">
+                <el-input v-if="['apriori', 'LIG', 'FP-growth'].includes(algorithm)" v-model="supportThreshold"
+                  placeholder="请输入支持度阈值" size="small" :style="{ width: '260px' }">
                   <template slot="prepend">支持度阈值：</template>
+                </el-input>
+                <el-input v-if="['apriori', 'LIG', 'FP-growth'].includes(algorithm)" v-model="confidenceThreshold"
+                  placeholder="请输入置信度阈值" size="small" :style="{ width: '260px' }">
+                  <template slot="prepend">置信度阈值：</template>
                 </el-input>
 
                 <el-button type="primary" @click="generateData">生成</el-button>
@@ -31,7 +39,7 @@
         </el-container>
       </div>
       <div>
-        <apriori :data="aprioriResults" />
+        <apriori :data="aprioriResults" :showAlert="showAlert"/>
       </div>
     </div>
   </div>
@@ -49,32 +57,66 @@ export default {
       algorithm: 'apriori',
       algorithmLists: ['apriori', 'LIG', 'FP-growth', 'k-means', 'k-medois', 'EM'],
       aprioriResults: [],
-      supportThreshold: 0.5
+      supportThreshold: 0.5,
+      confidenceThreshold: 0.5,
+      healthtypes: ['医疗', '财产', '保险'],
+      healthtype: '医疗',
+      showAlert: false
     }
   },
   methods: {
     generateData () {
       // 模拟生成数据的过程，根据支持度阈值过滤数据
-      let testData = [
-        { itemset: '{A}', support: 0.6 },
-        { itemset: '{B}', support: 0.7 },
-        { itemset: '{C}', support: 0.5 },
-        { itemset: '{D}', support: 0.4 },
-        { itemset: '{A, B}', support: 0.4 },
-        { itemset: '{A, C}', support: 0.3 },
-        { itemset: '{A, D}', support: 0.2 },
-        { itemset: '{B, C}', support: 0.35 },
-        { itemset: '{B, D}', support: 0.25 },
-        { itemset: '{C, D}', support: 0.3 },
-        { itemset: '{A, B, C}', support: 0.2 },
-        { itemset: '{A, B, D}', support: 0.15 },
-        { itemset: '{A, C, D}', support: 0.1 },
-        { itemset: '{B, C, D}', support: 0.2 },
-        { itemset: '{A, B, C, D}', support: 0.1 }
-        // 可以继续添加更多的数据
+      let hlthData = [
+        { itemset: '{发烧}', support: 0.3, confidence: 0.6 },
+        { itemset: '{头痛}', support: 0.25, confidence: 0.7 },
+        { itemset: '{胃痛}', support: 0.2, confidence: 0.5 },
+        { itemset: '{皮疹}', support: 0.15, confidence: 0.8 },
+        { itemset: '{发烧, 头痛}', support: 0.1, confidence: 0.4 },
+        { itemset: '{头痛, 胃痛}', support: 0.1, confidence: 0.6 },
+        { itemset: '{发烧, 胃痛, 皮疹}', support: 0.05, confidence: 0.7 },
+        { itemset: '{头痛, 皮疹, 发烧, 胃痛}', support: 0.03, confidence: 0.9 }
       ]
 
-      this.aprioriResults = testData.filter(item => item.support >= this.supportThreshold)
+      let insData = [
+        { itemset: '{商业保险}', support: 0.3, confidence: 0.6 },
+        { itemset: '{免费医疗保险}', support: 0.25, confidence: 0.7 },
+        { itemset: '{职工补偿保险}', support: 0.2, confidence: 0.5 },
+        { itemset: '{家庭成员保险}', support: 0.15, confidence: 0.8 },
+        { itemset: '{商业保险, 免费医疗保险}', support: 0.1, confidence: 0.4 },
+        { itemset: '{免费医疗保险, 职工补偿保险}', support: 0.1, confidence: 0.6 },
+        { itemset: '{商业保险, 免费医疗保险, 家庭成员保险}', support: 0.05, confidence: 0.7 },
+        { itemset: '{职工补偿保险, 家庭成员保险, 商业保险, 免费医疗保险}', support: 0.03, confidence: 0.9 }
+      ]
+
+      let asset = [
+        { itemset: '{室内，可以冲水}', support: 0.3, confidence: 0.6 },
+        { itemset: '{拥有自行车}', support: 0.25, confidence: 0.7 },
+        { itemset: '{室外公共卫生间，可以冲水}', support: 0.2, confidence: 0.5 },
+        { itemset: '{拥有汽车}', support: 0.15, confidence: 0.8 },
+        { itemset: '{室内，可以冲水, 拥有自行车}', support: 0.1, confidence: 0.4 },
+        { itemset: '{拥有自行车, 拥有汽车}', support: 0.1, confidence: 0.6 },
+        { itemset: '{室内，可以冲水, 拥有自行车, 拥有汽车}', support: 0.05, confidence: 0.7 },
+        { itemset: '{室外公共卫生间，可以冲水, 拥有汽车, 拥有自行车}', support: 0.03, confidence: 0.9 }
+
+      ]
+
+      if (this.healthtype === '医疗') {
+        this.aprioriResults = hlthData.filter(item => item.support >= this.supportThreshold && item.confidence >= this.confidenceThreshold)
+      } else if (this.healthtype === '保险') {
+        this.aprioriResults = insData.filter(item => item.support >= this.supportThreshold && item.confidence >= this.confidenceThreshold)
+      } else if (this.healthtype === '资产') {
+        this.aprioriResults = asset.filter(item => item.support >= this.supportThreshold && item.confidence >= this.confidenceThreshold)
+      }
+
+      if (this.aprioriResults.length === 0) {
+        console.log('没有找到匹配的结果')
+        console.log(this.showAlert)
+        this.showAlert = true
+        console.log(this.showAlert)
+      } else {
+        this.showAlert = false
+      }
     }
   }
 }
